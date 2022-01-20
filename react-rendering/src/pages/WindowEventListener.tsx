60113918edfefbeab78d9c7df393da8fac5d1808
ju1seo49@gmail.com <ju1seo49@gmail.com>
2022-01-21 00:31:13 +0900

    fdg

--- react-rendering/src/pages/WindowEventListener.tsx
+++ react-rendering/src/pages/WindowEventListener.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import {
-  useCallback,
-  useEffect,
-  useMemo,
-  useReducer,
-  useRef,
-  useState,
+    ReducerWithoutAction,
+    useCallback,
+    useEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
 } from "react";
 import { makeVar, useReactiveVar } from "@apollo/client";
 
@@ -21,43 +22,34 @@ export const WindowEventListener = (props: Props) => {
   // const myCount = useReactiveVar(countVar)
   // const countRef = useRef<number>(0);
 
-  // const [countFunc, setCountFunc] = useState<() => number>(() => {
-  //   return 0;
-  // });
-
-  // const [countReducer, setCountReducer] = useReducer((prev: number) => {
-  //   console.log("reduced!");
-  //   return prev + 1;
-  // }, 0);
-  const [countObjectFunc, setCountObjectFunc] = useState<{
-    setFunc: () => number;
-  }>({
-    setFunc: () => 0,
+  const [countFunc, setCountFunc] = useState<() => number>(() => () => {
+    return 0;
   });
 
-  const mouseDownEvent = () => {
+  // const [countState, setCountState] = useReducer((prevState: number, nextState: number) => {
+  //     return prevState + 1
+  // }, 0, () => {
+  //     return 1
+  // });
+
+    const mouseDownEvent = () => {
     // console.log(`mouse Down!! count: ${count}`);
     // console.log(`mouse Down!! countVar(): ${countVar()}`);
     // console.log(`mouse Down!! countRef: ${countRef.current}`);
     // console.log(`mouse Down!! countFunc: ${countFunc}`);
-    // console.log(`mouse Down!! countReducer: ${countReducer}`);
-    console.log(`mouse Down!! countObjectFunc: ${countObjectFunc.setFunc()}`);
+    // console.log(`mouse Down!! countState: ${countState}`);
 
     // setCount(count + 1);
-    // setCount((prev) => prev + 1);
     // countVar(countVar() + 1);
     // countRef.current = countRef.current + 1;
     // setCount(countRef.current + 1);
+        // setCount((prev) => prev + 1);
     // setCountFunc(() => {
     //   return 1;
     // });
-    // setCountReducer(countReducer + 1);
-    setCountObjectFunc({
-      setFunc: () => {
-        return countObjectFunc.setFunc() + 1;
-      },
-    });
+    // setCountState(90)
   };
+
 
   // const mouseDownEventWithUseCallback = useCallback(() => {
   //   setCount(count + 1);
@@ -67,9 +59,8 @@ export const WindowEventListener = (props: Props) => {
   // console.log(`count: ${count}`);
   // console.log(`countVar: ${myCount}`);
   // console.log(`countRef: ${countRef.current}`);
-  // console.log(`countFunc: ${countFunc}`);
-  // console.log(`countReducer: ${countReducer}`);
-  console.log(`countObjectFunc: ${countObjectFunc.setFunc()}`);
+  // console.log(`countFunc: ${countFunc()}`);
+  // console.log(`countState: ${countState}`);
 
   // useEffect(() => {
   //   countRef.current += 1;
@@ -81,6 +72,10 @@ export const WindowEventListener = (props: Props) => {
         onClick={() => {
           // setCount(count + 1);
           //   countVar(myCount + 1)
+          //   dispatch(2)
+            setCountFunc((prev) => () => {
+                return prev() + 1
+            })
         }}
       >
         Do Render with Count Up
